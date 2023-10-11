6df61b0bdab2aa2f65d871a1d0917ca5f3393e34
Alex Urmaza <aurmaza@albany.edu>
2023-10-11 19:41:16 -0400

    Cleaner 2sum solution

--- neetcode/Easy/twoSum.py
+++ neetcode/Easy/twoSum.py
@@ -9,4 +9,19 @@ class Solution:
                 return [numList[nums[i]] ,i]
             
             numList[needed] = i
-            
+#Cleaner way
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        dic[nums[0]] = 0
+        for i in range(1, len(nums)):
+            numNeeded = target - nums[i]
+            if numNeeded in dic.keys():
+                return [dic[numNeeded],i]
+            else:
+                dic[nums[i]] = i

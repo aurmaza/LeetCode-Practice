e1e54b1b1cd0e9693dc001ec42f545256ea1a5c8
Alex Urmaza <aurmaza@albany.edu>
2023-10-12 19:39:36 -0400

    neetsolution

--- neetcode/Medium/validSuduku.py
+++ neetcode/Medium/validSuduku.py
@@ -41,6 +41,27 @@ class Solution(object):
                         boxCheck.add(val)
                     
         return True
-        
+    
+    # neet solution
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        boxes = collections.defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                if(board[i][j] in rows[i] or
+                   board[i][j] in cols[j] or
+                   board[i][j] in boxes[(i//3, j//3)] ): return False
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                boxes[(i//3, j//3)].add(board[i][j])
+        return True  
 
         

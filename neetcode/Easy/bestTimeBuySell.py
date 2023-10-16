f9f77fde69b879a5516fd333f4aaa84840dba88c
Alex Urmaza <aurmaza@albany.edu>
2023-10-15 21:52:50 -0400

    ig

--- neetcode/Easy/bestTimeBuySell.py
+++ neetcode/Easy/bestTimeBuySell.py
@@ -4,26 +4,12 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices) == 1: return 0
-        if len(prices) == 2:
-            temp = prices[1] - prices[0]
-            if(temp > 0):
-                return temp
-            else:
-                return 0
-        ///
-        
         maxP=0    
-        l, r = 0, 1
+        l, r = 0,1
         while r < len(prices):
-            # If left price is less than right
-            #Profit is possible
-            #Don't increment left pointer
-            if prices[l] < prices[r]:
-                maxP=max(maxP,prices[r]-prices[l])
-            #Profit is not possible, move the left pointer to where right pointer is
+            if prices[l] <= prices[r]:
+                maxP = max(maxP, prices[r]-prices[l])
             else:
                 l=r
-            #Increment right pointer regardless
             r+=1
         return maxP

e8e49e61bd5f8b15d6a7422ba28be216af6cc103
Alex Urmaza <aurmaza@albany.edu>
2023-10-23 21:34:08 -0400

    no need to search for dict.values( ) for max freq

--- neetcode/Medium/charReplacement.py
+++ neetcode/Medium/charReplacement.py
@@ -19,3 +19,30 @@ class Solution(object):
 
             res = max(res, r-l+1)
         return res
+
+
+# Fasterclass Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        l,r = 0,0
+        count = {}
+        res = 0
+        maxF = 0
+
+        while r < len(s):
+            count[s[r]] = count.get(s[r], 0) + 1
+            maxF = max(maxF, count[s[r]])
+
+            if (r-l+1) - maxF > k:
+                count[s[l]] -=1
+                l+=1
+            res = max(res, r-l+1)
+
+            r+=1
+        return res
+
+

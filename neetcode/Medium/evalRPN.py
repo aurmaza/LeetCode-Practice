6e5ba07b1f1561b744471f072d41ac0adb825a23
Alex Urmaza <aurmaza@albany.edu>
2023-10-31 19:09:21 -0400

    ohhh i understand

---
+++ neetcode/Medium/evalRPN.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        
+        for t in tokens:
+            if t.isdigit() or (t[0] == '-' and t[1:].isdigit()):
+                stack.append(int(t))
+            elif len(stack) > 1:
+                element1 = stack.pop()
+                element2 = stack.pop()
+                res = 0
+                if t == "+":
+                    res = element1 + element2
+                elif t =="*":
+                    res = element1*element2
+                elif t == "/":
+                    if element2 * element1<0 and element2 % element1 != 0:
+                        res = element2//element1 + 1
+                    else: 
+                        res = element2//element1
+                else:
+                    res = element2-element1
+                stack.append(res)
+        return stack[0]

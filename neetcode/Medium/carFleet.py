c3c85da15cd72c3e4645a870e446021b39934fe4
Alex <alexmurmaza@gmail.com>
2023-11-08 19:31:54 -0500

    notes

--- neetcode/Medium/carFleet.py
+++ neetcode/Medium/carFleet.py
@@ -11,7 +11,7 @@ class Solution(object):
         stack = []
         #reverse sorted order        
         for p,s in sorted(pair)[::-1]:
-            stack.append((target-p) / s)
+            stack.append((target-p) / s)#time it takes for car at p to arrive at target
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)

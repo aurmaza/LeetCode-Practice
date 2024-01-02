930b0b76b59d31b606222a6be3a9e736cf0cb5b1
Alex <alexmurmaza@gmail.com>
2024-01-01 19:15:20 -0500

    redo

--- neetcode/Easy/slowFast.py
+++ neetcode/Easy/slowFast.py
@@ -12,3 +12,25 @@ class Solution(object):
                 return True
         return False
                 
+                
+                
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        s = f = head
+        
+        while f and f.next:
+            f = f.next.next
+            s = s.next
+            if s == f:
+                return True
+        return False
+
+
+        
+        #redo, forgot the f.next part.

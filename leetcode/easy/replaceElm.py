ec03aec23922d1cf0e89f37168a5b33990ccd55e
aurmaza <89319729+aurmaza@users.noreply.github.com>
2024-01-07 19:29:40 -0500

    Update replaceElm.py

--- leetcode/easy/replaceElm.py
+++ leetcode/easy/replaceElm.py
@@ -1,7 +1,6 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         l = len(arr) - 1
-        largest = -2
         past = -1
         for n in range(l, -1, -1):
             temp = past

fda14ab8684fc150b4b2ce7d4d58de3b184b2863
aurmaza <89319729+aurmaza@users.noreply.github.com>
2024-01-15 02:10:23 -0500

    Create islandPerimeter.py

---
+++ neetcode/Easy/islandPerimeter.py
@@ -0,0 +1,28 @@
+class Solution:
+    
+
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        visited = set()
+
+        def dfs(i,j):
+            # If edge or empty space at i j, return 1
+            if i>=len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+                return 1
+            #If already visited, return 0
+            if (i,j) in visited:
+                return 0
+            #Else, visiting i,j
+            #find i-j peremeter
+            visited.add((i,j))
+            perimeter = dfs(i, j+1)
+            perimeter+=dfs(i+1,j)
+            perimeter+=dfs(i, j-1)
+            perimeter+=dfs(i-1,j)
+            return perimeter
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    return dfs(i,j)
+
+

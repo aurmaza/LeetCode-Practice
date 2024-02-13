347c42b112bb72e20a8fd33637f31a58a3de3e16
aurmaza <89319729+aurmaza@users.noreply.github.com>
2024-02-13 15:05:41 -0500

    Create trib.py

---
+++ neetcode/Easy/trib.py
@@ -0,0 +1,17 @@
+class Solution:
+    Memo = {}
+    def tribonacci(self, n: int) -> int:
+        print(self.Memo)
+        if n in self.Memo:
+            return self.Memo[n]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 1
+        self.Memo[n] = (
+            self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        )
+        return self.Memo[n]
+

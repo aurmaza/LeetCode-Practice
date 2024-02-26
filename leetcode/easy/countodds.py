42dfe7720107a223a634e22612743d4350520a95
aurmaza <89319729+aurmaza@users.noreply.github.com>
2024-02-25 22:11:11 -0500

    Create countodds.py

---
+++ leetcode/easy/countodds.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        length = (high-low) + 1
+        count = length//2
+        if low % 2 and length % 2:
+            count+=1
+        return count
+        

3f1f85454a945dc75f90ee394592336a72c518f8
aurmaza <89319729+aurmaza@users.noreply.github.com>
2024-03-16 00:51:23 -0400

    Update findcontigarray10.py

--- leetcode/medium/findcontigarray10.py
+++ leetcode/medium/findcontigarray10.py
@@ -8,3 +8,10 @@ class Solution:
             if count in dic: m = max(m, i - dic[count])
             else: dic[count] = i
         return m
+#Keep a dict that holds the first index where each unique 'count' value was encountered
+#if the same count is seen again, the dict help us find start of the subarray that balances 0 and 1 
+#to the curr index
+#so its the distance between that index and the current index
+# if we encounter a count that already exists, that means
+#from where it is first encountered to the current
+#it has been rebalanced and the subarray from curr to index of the first encounter is balanced
